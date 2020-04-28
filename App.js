bb693093bfc708ad950c9a1a54675cd0a395ba60
Zaeem Naveed <zaeemnaveed5@gmail.com>
2020-04-29 01:12:28 +0500

    First Event handler added

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,AsyncStorage} from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
+  
+  render(){
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +19,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
